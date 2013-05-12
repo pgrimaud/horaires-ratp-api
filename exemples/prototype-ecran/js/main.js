3c6ece32be2aad397861f8cda2217d96cc33adec
rhannequin <remy.hannequin@gmail.com>
2013-05-13 00:08:08 +0200

    [fix] Show '0' before hours and minutes

--- exemples/prototype-ecran/js/main.js
+++ exemples/prototype-ecran/js/main.js
@@ -6,8 +6,11 @@ $(window).load(function() {
     var req = $.getJSON('http://api-ratp.pierre-grimaud.fr/rer/b/arcueil+cachan/cdg+mitry');
     req.done(function (data) {
       data.destination = data.destination.replace(' ', '<br />');
-      var date = new Date();
-      data.current_time = date.getHours() + ':' + date.getMinutes();
+      var date = new Date(),
+          hours = date.getHours(),
+          minutes = date.getMinutes();
+      data.current_time = (hours < 10 ? '0' + hours : hours) + ':' +
+                          (minutes < 10 ? '0' + minutes : minutes);
       $content.html(template(data));
     });
     req.error(function (err) {
