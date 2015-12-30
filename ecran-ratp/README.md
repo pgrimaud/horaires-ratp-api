411cdb1c635b14b77ae1492af3600cefed61deaa
Pierre Grimaud <pierre@shopping-flux.com>
2015-12-30 22:00:57 +0100

    Prototype écran

---
+++ ecran-ratp/README.md
@@ -0,0 +1,28 @@
+
+# Écran RATP
+
+	
+
+![Screenshot écran ratp](http://i.imgur.com/m9wJIzph.png)
+
+## Informations
+
+Inspiré du design de la RATP, cet écran permet de visualiser les prochains horaires d'une station d'une ligne vers une destination.
+
+## Configuration
+
+Editer les valeurs des lignes suivantes du fichier js/main.js
+
+	Ligne 7 : var schedules_url = '';
+	Ligne 8 : var traffic_url = '';
+
+*Exemple :*  
+	
+	var schedules_url = 'http://api-ratp.pierre-grimaud.fr/v2/rers/b/stations/arcueil+cachan?destination=4';
+    var traffic_url = 'http://api-ratp.pierre-grimaud.fr/v2/traffic/rers/b';
+    
+Ce qui affichera les horaires des prochains trains du **RER B** en direction de **Charles De Gaulle - Mitry Claye** à la station **Arcueil-Cachan** ainsi que l'état du trafic sur la ligne.
+
+## Données
+
+Pour obtenir les informations d'une autre ligne de transport, d'une autre station ou d'une autre direction, consulter la documention de l'API  [horaires-ratp-api](http://github.com/pgrimaud/horaires-ratp-api).
--- ecran-ratp/js/main.js
+++ ecran-ratp/js/main.js
@@ -3,23 +3,29 @@ $(window).load(function() {
       $content = $('#content');
 
   var getData = function() {
-    var req = $.getJSON('http://api-ratp.pierre-grimaud.fr/v2/metros/1/stations/87?destination=6');
 
-    req.done(function (data) {
-      data.destination = data.response.informations.destination.name.replace(' - ', '<br />');
-      data.line = data.response.informations.line;
-      data.type = data.response.informations.type;
-      data.horaires = data.response.schedules;
+    var schedules_url = 'http://api-ratp.pierre-grimaud.fr/v2/rers/b/stations/arcueil+cachan?destination=4';
+    var traffic_url = 'http://api-ratp.pierre-grimaud.fr/v2/traffic/rers/b';
+
+    $.when($.getJSON(schedules_url), $.getJSON(traffic_url)).done(function(schedules, traffic) {
+      var data = new Object();
+      data.traffic = traffic[0].response.message;
+
+      var response = schedules[0].response.informations.destination.name.replace(' - ', '<br />');
+      data.line = schedules[0].response.informations.line;
+      data.type = schedules[0].response.informations.type;
+      data.horaires = schedules[0].response.schedules;
+      data.destination = schedules[0].response.informations.destination.name
+      data.station = schedules[0].response.informations.station.name;
 
       var date = new Date(),
           hours = date.getHours(),
           minutes = date.getMinutes();
       data.current_time = (hours < 10 ? '0' + hours : hours) + ':' +
                           (minutes < 10 ? '0' + minutes : minutes);
-      $content.html(template(data));
-    });
-    req.error(function (err) {
-      console.error(err);
+
+      $content.html(template(data));$
+
     });
   };
 
