d58bd94b4ba7087cc7e6699c750fd752fe21f18c
Pierre Grimaud <pierre@shopping-flux.com>
2016-07-10 18:59:44 +0200

    fix ratp screen

--- ecran-ratp/js/main.js
+++ ecran-ratp/js/main.js
@@ -8,14 +8,14 @@ $(window).load(function() {
     var traffic_url = 'http://api-ratp.pierre-grimaud.fr/v2/traffic/rers/b';
 
     $.when($.getJSON(schedules_url), $.getJSON(traffic_url)).done(function(schedules, traffic) {
-      var data = new Object();
+      var data = {};
       data.traffic = traffic[0].response.message;
 
       var response = schedules[0].response.informations.destination.name.replace(' - ', '<br />');
       data.line = schedules[0].response.informations.line;
       data.type = schedules[0].response.informations.type;
       data.horaires = schedules[0].response.schedules;
-      data.destination = schedules[0].response.informations.destination.name
+      data.destination = schedules[0].response.informations.destination.name;
       data.station = schedules[0].response.informations.station.name;
 
       var date = new Date(),
@@ -24,7 +24,7 @@ $(window).load(function() {
       data.current_time = (hours < 10 ? '0' + hours : hours) + ':' +
                           (minutes < 10 ? '0' + minutes : minutes);
 
-      $content.html(template(data));$
+      $content.html(template(data));
 
     });
   };
