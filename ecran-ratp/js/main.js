77dc841354ef56b403ff6b0e9832f4bb9cf7e5ed
Pierre Grimaud <pierre@shopping-flux.com>
2017-03-16 00:01:15 +0100

    update screen with v3

--- ecran-ratp/js/main.js
+++ ecran-ratp/js/main.js
@@ -1,37 +1,37 @@
-$(function() {
-  var template = _.template($('#page-template').html()),
-      $content = $('#content');
+$(function () {
+    var template = _.template($('#page-template').html()),
+        $content = $('#content');
 
-  var getData = function() {
+    var getData = function () {
 
-    var schedules_url = 'https://api-ratp.pierre-grimaud.fr/v2/rers/b/stations/arcueil+cachan?destination=4';
-    var traffic_url = 'https://api-ratp.pierre-grimaud.fr/v2/traffic/rers/b';
+        var schedules_url = 'https://api-ratp.pierre-grimaud.fr/v3/schedules/rers/b/arcueil+cachan/A';
+        var traffic_url = 'https://api-ratp.pierre-grimaud.fr/v3/traffic/rers/b';
 
-    $.when($.getJSON(schedules_url), $.getJSON(traffic_url)).done(function(schedules, traffic) {
+        $.when($.getJSON(schedules_url), $.getJSON(traffic_url)).done(function (schedules, traffic) {
             var date = new Date(),
                 hours = date.getHours(),
                 minutes = date.getMinutes(),
                 current_time = (hours < 10 ? '0' + hours : hours) + ':' + (minutes < 10 ? '0' + minutes : minutes);
 
-            var trafficResponse = traffic[0].response,
-                scheduleResponse = schedules[0].response,
-                scheduleResponseInformation = scheduleResponse.informations;
+            var trafficResponse = traffic[0].result,
+                scheduleResponse = schedules[0].result,
+                scheduleResponseInformation = scheduleResponse;
 
             var data = {
                 traffic: trafficResponse.message,
-                line: scheduleResponseInformation.line,
-                type: scheduleResponseInformation.type,
+                line: 'B',
+                type: 'rer',
                 horaires: scheduleResponse.schedules,
-                destination: scheduleResponseInformation.destination.name,
-                station: scheduleResponseInformation.station.name,
+                destination: scheduleResponseInformation.destination,
+                station: 'Arcueil Cachan',
                 current_time: current_time
             };
 
-      $content.html(template(data));
+            $content.html(template(data));
 
-    });
-  };
+        });
+    };
 
-  getData();
-  setInterval(getData,20000);
+    getData();
+    setInterval(getData, 20000);
 });
